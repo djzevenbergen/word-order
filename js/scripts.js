a87b990d2846c33af77c913fcb354f0b56c7001d
djzevenbergen <djzevenbergen@gmail.com>
2020-03-30 16:41:02 -0700

    start javascript

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#textForm").submit(function (event) {
+    event.preventDefault();
+    var wholeParagraph = $("input#sentence").val();
+
+    var paragraphArrays = wholeParagraph.split(' ');
+    var seenWords = [];
+
+    paragraphArrays.forEach(function (paragraphArray) {
+      if (seenWords.includes(paragraphArray) === false) {
+        seenWords.push(paragraphArray);
+      }
+
+
+    });
+    alert(seenWords);
+
+  });
+
+
+
+});
